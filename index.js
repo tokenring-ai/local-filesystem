dc0c317ce9d1660c945af60daab8710b6acfc22f
Mark Dierolf <mdierolf@cloudquote.io>
2025-07-19 08:40:04 -0700

    Configured biome, and added it to the pre-commit checks

--- index.js
+++ index.js
@@ -1,8 +1,6 @@
-
 export { default as LocalFileSystemService } from "./LocalFileSystemService.js";
 
-
 export const name = "@token-ring/local-filesystem";
-export const description = "Service that add file contents or file names to the chat memory.";
+export const description =
+	"Service that add file contents or file names to the chat memory.";
 export const version = "0.1.0";
-
